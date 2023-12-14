00deb8fe9d64e97b50d4d21c47cc0be00e8896f1
amishak <amishak@damcogroup.com>
2023-12-14 16:43:00 +0530

    first commit

---
+++ TodoList/mainApp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from .forms import TaskForm  # Assuming you have a TaskForm defined in forms.py
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')  # Redirect to the task list page
+    else:
+        form = TaskForm(instance=task)
+    
+    return render(request, 'task_form.html', {'form': form, 'task': task, 'is_editing': True})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'task_confirm_delete.html', {'task': task})
